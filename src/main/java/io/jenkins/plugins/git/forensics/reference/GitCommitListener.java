9c08ad83731e166c289fa95bc0d1756f07a52e49
arne <schoenta@hm.edu>
2019-06-06 12:51:20 +0200

    auslagern in RepositoryCallback

--- src/main/java/io/jenkins/plugins/git/forensics/reference/GitCommitListener.java
+++ src/main/java/io/jenkins/plugins/git/forensics/reference/GitCommitListener.java
@@ -7,16 +7,20 @@ import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.model.listeners.SCMListener;
 import hudson.plugins.git.GitSCM;
+import hudson.remoting.VirtualChannel;
 import hudson.scm.SCM;
 import hudson.scm.SCMRevisionState;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.LogCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.jenkinsci.plugins.gitclient.GitClient;
+import org.jenkinsci.plugins.gitclient.RepositoryCallback;
 
 import javax.annotation.CheckForNull;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -45,36 +49,64 @@ public class GitCommitListener extends SCMListener {
             }
         }
 
-        List<String> newCommits = new ArrayList<>();
+
 
         // Build Repo
         GitSCM gitSCM = (GitSCM) scm;
         EnvVars environment = build.getEnvironment(listener);
         GitClient gitClient = gitSCM.createClient(listener, environment, build, workspace);
-        // TODO find not deprecated way?
-        Repository repo = gitClient.getRepository();
-        Git git = new Git(repo);
 
-        // Determine new commits to log since last build
-        String localBranch = gitSCM.getParamLocalBranch(build,listener);
-        LogCommand logCommand = git.log().add(repo.resolve(localBranch));
-        Iterable<RevCommit> commits = logCommand.call();
-        Iterator<RevCommit> iterator = commits.iterator();
-        RevCommit next;
-        while (iterator.hasNext()) {
-            next = iterator.next();
-            String commitId = next.getId().toString();
-            // If latestReversionOfPreviousCommit is null, all commits will be added.
-            if (commitId.equals(latestReversionOfPreviousCommit)) {
-                break;
-            }
-            newCommits.add(commitId);
-        }
+//        Repository repo = gitClient.getRepository();
 
         // Save new commits
-        GitCommit gitCommit = new GitCommit(build);
-        gitCommit.getGitCommitLog().getReversions().addAll(newCommits);
+        GitCommit gitCommit = gitClient.withRepository(new GitCommitCall(build, gitSCM.getParamLocalBranch(build,listener), latestReversionOfPreviousCommit));
+
+//        gitCommit.getGitCommitLog().getReversions().addAll(newCommits);
         build.addAction(gitCommit);
+    }
+
+    static class GitCommitCall implements RepositoryCallback<GitCommit> {
+
+        private static final long serialVersionUID = -5980402198857923793L;
+        private transient final Run<?,?> build;
+        private final String localBranch;
+        private final String latestReversionOfPreviousCommit;
+
+        public GitCommitCall(Run<?, ?> build, String localBranch, String latestReversionOfPreviousCommit) {
+            this.build = build;
+            this.localBranch = localBranch;
+            this.latestReversionOfPreviousCommit = latestReversionOfPreviousCommit;
+        }
+
+        @Override
+        public GitCommit invoke(Repository repo, VirtualChannel channel) throws IOException, InterruptedException {
+            GitCommit result = new GitCommit(build);
+            List<String> newCommits = new ArrayList<>();
+            Git git = new Git(repo);
+
+            try {
+                // Determine new commits to log since last build
+                LogCommand logCommand = git.log().add(repo.resolve(localBranch));
+                Iterable<RevCommit> commits = null;
+                commits = logCommand.call();
+                Iterator<RevCommit> iterator = commits.iterator();
+                RevCommit next;
+                while (iterator.hasNext()) {
+                    next = iterator.next();
+                    String commitId = next.getId().toString();
+                    // If latestReversionOfPreviousCommit is null, all commits will be added.
+                    if (commitId.equals(latestReversionOfPreviousCommit)) {
+                        break;
+                    }
+                    newCommits.add(commitId);
+                }
+            } catch (GitAPIException e) {
+                e.printStackTrace();
+            }
+
+            result.getGitCommitLog().getReversions().addAll(newCommits);
+            return result;
+        }
     }
 
 }
