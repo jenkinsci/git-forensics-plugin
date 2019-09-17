d88e962797095e1fbfa6a7576cf7bef5e345f01b
Ulli Hafner <ullrich.hafner@gmail.com>
2019-09-17 22:47:23 +0200

    Improve docker restart.

--- go.sh
+++ go.sh
@@ -9,8 +9,10 @@ echo "Installing plugin in $JENKINS_HOME"
 rm -rf $JENKINS_HOME/plugins/git-forensics-plugin*
 cp -fv target/git-forensics.hpi $JENKINS_HOME/plugins/git-forensics.jpi
 
+CURRENT_UID="$(id -u):$(id -g)"
+export CURRENT_UID
 IS_RUNNING=$(docker-compose ps -q jenkins-master)
 if [[ "$IS_RUNNING" != "" ]]; then
-    echo "Restarting Jenkins..."
     docker-compose restart
+    echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
 fi
