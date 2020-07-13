9fa474754c4998fc2e364881f283c679d9a83a32
rathaya <giulia.delbravo@gmx.de>
2020-07-13 06:12:12 -0700

    Added CommitCollector class.

---
+++ src/main/java/io/jenkins/plugins/forensics/git/miner/CommitCollector.java
@@ -0,0 +1,52 @@
+package io.jenkins.plugins.forensics.git.miner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.LogCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+/**
+ * Collects all commits for a git repository.
+ *
+ * @author Giulia Del Bravo
+ */
+public class CommitCollector {
+    private final Repository repository;
+    private final Git git;
+    //Assign later when incremental analysis is implemented.
+    private String latestRevisionOfPreviousCommit;
+
+    CommitCollector(final Repository repository, final Git git) {
+        this.repository = repository;
+        this.git = git;
+        latestRevisionOfPreviousCommit = null;
+    }
+
+    ArrayList<String> findAllCommits() throws IOException, GitAPIException {
+        ObjectId headCommit = repository.resolve(Constants.HEAD);
+        LogCommand logCommand = git.log().add(headCommit);
+        Iterable<RevCommit> commits;
+        commits = logCommand.call();
+        Iterator<RevCommit> iterator = commits.iterator();
+        RevCommit next;
+        ArrayList<String> newCommits = new ArrayList<>();
+        while (iterator.hasNext()) {
+            next = iterator.next();
+            String commitId = next.getId().getName();
+            if (commitId.equals(latestRevisionOfPreviousCommit)) {
+                break;
+            }
+            newCommits.add(commitId);
+        }
+
+        return newCommits;
+    }
+}
