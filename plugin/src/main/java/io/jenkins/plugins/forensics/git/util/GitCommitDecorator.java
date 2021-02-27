a0203b7c98aaac4adfc7ebc86c7641eaed9335f3
Ulli Hafner <ullrich.hafner@gmail.com>
2021-02-27 21:12:25 +0100

    Move link generation to `GitRepositoryBrowser`.

--- plugin/src/main/java/io/jenkins/plugins/forensics/git/util/GitCommitDecorator.java
+++ plugin/src/main/java/io/jenkins/plugins/forensics/git/util/GitCommitDecorator.java
@@ -2,15 +2,10 @@ package io.jenkins.plugins.forensics.git.util;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Collections;
-import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 
-import edu.hm.hafner.util.Generated;
-
-import hudson.plugins.git.GitChangeSet;
 import hudson.plugins.git.browser.GitRepositoryBrowser;
 import hudson.scm.RepositoryBrowser;
 
@@ -43,7 +38,7 @@ public class GitCommitDecorator extends GitCommitTextDecorator {
 
     private Optional<String> createLink(final String id) {
         try {
-            URL link = browser.getChangeSetLink(new DummyChangeSet(id));
+            URL link = browser.getChangeSetLink(id);
             if (link != null) {
                 return Optional.of(a().withText(asText(id)).withHref(link.toString()).render());
             }
@@ -52,40 +47,5 @@ public class GitCommitDecorator extends GitCommitTextDecorator {
             // ignore and return nothing
         }
         return Optional.empty();
-    }
-
-    // TODO: move this class and the implementation to the Git plugin
-    private static class DummyChangeSet extends GitChangeSet {
-        private final String id;
-
-        DummyChangeSet(final String id) {
-            super(Collections.emptyList(), false);
-            this.id = id;
-        }
-
-        @Override
-        public String getId() {
-            return id;
-        }
-
-        @Override @Generated
-        public boolean equals(final Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            if (!super.equals(o)) {
-                return false;
-            }
-            DummyChangeSet that = (DummyChangeSet) o;
-            return Objects.equals(id, that.id);
-        }
-
-        @Override @Generated
-        public int hashCode() {
-            return Objects.hash(super.hashCode(), id);
-        }
     }
 }
